579308b21d59428d8224ee38d550ce5fdda8479e
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-11-30 17:50:38 +0330

    feat: Home page just initialized and style file just initialized into the project.

---
+++ src/ui/Pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+const style = require('./style.scss');
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import  { loginUserAction } from 'Store/User/actions';
+
+
+interface IProps {
+}
+interface IState {
+}
+
+class Home_Page extends React.Component<IProps, IState> {
+  constructor(props: IProps){
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  public render() {
+    return (
+      <div>
+      Zooom Productions
+      </div>
+    );
+  }
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+});
+
+export const Home =  connect(null, mapDispatchToProps)(Home_Page);
