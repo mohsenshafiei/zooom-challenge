0eb7426c06fbf54f69ae3c8438ccc4bb26526ba6
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-11-30 21:24:36 +0330

    feat: just a sample user api service added to the project but later should remove it.

---
+++ src/services/api/User/index.ts
@@ -0,0 +1,16 @@
+import { Client, HttpMethod } from 'Util/Api';
+import {
+  ILoginRequest,
+} from "./model";
+
+const login = (data: ILoginRequest) => {
+  return Client.request<ILoginRequest>(HttpMethod.GET, 'user/login', data)
+    .then(res => res)
+    .catch(error => error);
+};
+
+const User =  {
+  login,
+};
+
+export default User;
