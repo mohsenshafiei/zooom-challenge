77670694984700d3cc7a398f768861fa5cfc01a4
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-12-04 09:24:26 +0330

    refactor: some minor changes in resolve paths of modules.

--- src/store/User/saga.ts
+++ src/store/User/saga.ts
@@ -1,6 +1,6 @@
 import { takeEvery, call } from 'redux-saga/effects';
 import  { ActionTypes } from './actionTypes';
-import User from 'Services/api/User';
+import User from 'services/api/User';
 import { ILoginUser } from "./models";
 
 function* loginUser(action: ILoginUser) {
