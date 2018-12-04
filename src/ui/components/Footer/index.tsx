5065907a06ed0b414a2ccd35133934632ece0f6d
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-12-04 09:31:44 +0330

    feat: footer component just added to the project.

---
+++ src/ui/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+const style = require('./style.scss');
+
+interface IFooterProps {
+}
+interface IFooterState {
+  title: string;
+}
+
+export class Footer extends React.PureComponent<IFooterProps, IFooterState> {
+  render() {
+    return (
+      <footer className={style.footer}>
+        <a className={style.copyRight} href="#">All Rights Reserved &copy Zooom Productions | 2018</a>
+      </footer>
+    );
+  }
+}
