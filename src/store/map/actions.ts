039a5f5e5238ddb359daa11b71841cca84a323eb
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-05 21:20:18 +0330

    feat: setFilteration action creator just added to the actions.

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -2,11 +2,19 @@ import { ActionTypes } from "./actionTypes";
 import {
   ISetLocation,
   ISetLocationPayload,
+  ISetFilter,
 } from "./models";
 
 export const setLocationAction = (payload: ISetLocationPayload): ISetLocation => {
   return {
     type: ActionTypes.SET_LOCATION,
     coordination: payload.coordination
+  }
+};
+
+export const setFilteration = (option: number): ISetFilter => {
+  return {
+    type: ActionTypes.SET_FILTER,
+    option: option
   }
 };
