1b2d5e2cf329757d3539866a33c2ac147dcfbd92
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-05 11:21:27 +0330

    feat: props just added to the place component.

--- src/ui/components/Place/index.tsx
+++ src/ui/components/Place/index.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 const style = require('./style.scss');
+import { LngLatLike } from 'mapbox-gl';
 
 interface IPlaceProps {
-  headline?: string,
-  description?: string,
-  address?: string,
-  zip?: string,
-  country?: string,
-  startDate?: string,
-  EndDate?: string,
-  category?: string,
-  location?: string,
-  selectLocation?: (coordination: Array<number>) => void
+  headline: string,
+  description: string,
+  address: string,
+  zip: string,
+  country: string,
+  startDate: string,
+  endDate: string,
+  category: string,
+  location: LngLatLike,
+  selectLocation: (coordination: LngLatLike) => void
 }
 interface IPlaceState {
   title: string;
@@ -22,21 +23,21 @@ export class Place extends React.PureComponent<IPlaceProps, IPlaceState> {
     super(props);
     this.select = this.select.bind(this);
   }
-  select(coordination: Array<number>) {
+  select(coordination: LngLatLike) {
     this.props.selectLocation(coordination);
   }
   render() {
     return (
-      <div className={style.place} onClick={(e) => { this.select([33, 44]) }}>
+      <div className={style.place} onClick={(e) => { this.select(this.props.location) }}>
         <div className={style.informationContainer}>
-          <p>Headline</p>
-          <p>Description</p>
-          <p>Address</p>
-          <p>Zip</p>
-          <p>Country</p>
-          <p>Start Date</p>
-          <p>End Date</p>
-          <p>Category</p>
+          <p className={style.headline}>{this.props.headline}</p>
+          <p>{this.props.description}</p>
+          <p>{this.props.address}</p>
+          <p>{this.props.zip}</p>
+          <p>{this.props.country}</p>
+          <p>{this.props.startDate}</p>
+          <p>{this.props.endDate}</p>
+          <p>{this.props.category}</p>
         </div>
       </div>
     );
