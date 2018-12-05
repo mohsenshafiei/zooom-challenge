a6b859f849466b5927a1cb2b6c179db4462a5c99
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-05 21:23:08 +0330

    feat: setFilter just added to the sagas.

--- src/store/map/saga.ts
+++ src/store/map/saga.ts
@@ -1,12 +1,18 @@
 import {takeEvery, put} from 'redux-saga/effects';
 import  { ActionTypes } from './actionTypes';
-import {ISetLocation} from "./models";
+import {ISetFilter, ISetLocation} from "./models";
 
 function* setLocation(action: ISetLocation) {
   yield put({ type: ActionTypes.SET_LOCATION_PENDING });
   yield put({ type: ActionTypes.SET_LOCATION_FULFILLED, payload: action.coordination });
 }
 
+function* setFilter(action: ISetFilter) {
+  yield put({ type: ActionTypes.SET_FILTER_PENDING });
+  yield put({ type: ActionTypes.SET_FILTER_FULFILLED, payload: action.option});
+}
+
 export default [
   takeEvery(ActionTypes.SET_LOCATION, setLocation),
+  takeEvery(ActionTypes.SET_FILTER, setFilter),
 ];
