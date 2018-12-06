a2453c142aaeb58f16c0951040641cb1efc2c60d
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-06 21:21:19 +0330

    feat: Navigation Drawer component just added to the project but it needs some minor refactoring.

---
+++ src/ui/components/NavigationDrawer/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+const style = require('./style.scss');
+
+
+interface INavigationDrawerProps {
+  children: React.ReactNode,
+  visible: boolean
+}
+interface INavigationDrawerState {
+  title: string;
+}
+
+export class NavigationDrawer extends React.PureComponent<INavigationDrawerProps, INavigationDrawerState> {
+  constructor(props: INavigationDrawerProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <div className={this.props.visible ? style.openNavigationDrawer : style.closeNavigationDrawer}>
+          <div className={style.column}>
+            {this.props.children}
+            </div>
+        </div>
+      </>
+    );
+  }
+}
