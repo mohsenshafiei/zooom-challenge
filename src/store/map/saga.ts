f7d210f46cf05bcd3753b6ff240cdd8d529f4762
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-06 15:51:44 +0330

    feat: search location saga function just added to the map store module.

--- src/store/map/saga.ts
+++ src/store/map/saga.ts
@@ -1,6 +1,12 @@
-import {takeEvery, put} from 'redux-saga/effects';
+import {takeEvery, put, call, takeLatest} from 'redux-saga/effects';
 import  { ActionTypes } from './actionTypes';
-import {ICloseDetails, ISetFilter, ISetLocation} from "./models";
+import {
+  ICloseDetails,
+  ISetFilter,
+  ISetLocation,
+  ISearchLocation
+} from "./models";
+import Map from "services/api/map";
 
 function* setLocation(action: ISetLocation) {
   yield put({ type: ActionTypes.SET_LOCATION_PENDING });
@@ -17,8 +23,28 @@ function* closeDetails(action: ICloseDetails) {
   yield put({ type: ActionTypes.CLOSE_DETAILS_FULFILLED });
 }
 
+function* search(action: ISearchLocation) {
+  yield put({ type: ActionTypes.SEARCH_LOCATIONS_PENDING});
+  const response = yield call(Map.search, {
+    location: action.payload,
+  });
+  if (response.status === 200) {
+    yield put({
+      type: ActionTypes.SEARCH_LOCATIONS_FULFILLED,
+      payload: response.data.features
+    });
+  } else {
+    yield put({
+      type: ActionTypes.SEARCH_LOCATIONS_REJECTED,
+    });
+  }
+
+
+}
+
 export default [
   takeEvery(ActionTypes.SET_LOCATION, setLocation),
   takeEvery(ActionTypes.SET_FILTER, setFilter),
   takeEvery(ActionTypes.CLOSE_DETAILS, closeDetails),
+  takeLatest(ActionTypes.SEARCH_LOCATIONS, search),
 ];
