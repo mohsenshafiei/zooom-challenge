2fc733102b3f04fbee9c1995672a64e34de32d37
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-06 11:48:37 +0330

    feat: close details and show details functions just added to our map sagas.

--- src/store/map/saga.ts
+++ src/store/map/saga.ts
@@ -1,10 +1,11 @@
 import {takeEvery, put} from 'redux-saga/effects';
 import  { ActionTypes } from './actionTypes';
-import {ISetFilter, ISetLocation} from "./models";
+import {ICloseDetails, ISetFilter, ISetLocation} from "./models";
 
 function* setLocation(action: ISetLocation) {
   yield put({ type: ActionTypes.SET_LOCATION_PENDING });
   yield put({ type: ActionTypes.SET_LOCATION_FULFILLED, payload: action.coordination });
+  yield put({ type: ActionTypes.SHOW_DETAILS_FULFILLED });
 }
 
 function* setFilter(action: ISetFilter) {
@@ -12,7 +13,12 @@ function* setFilter(action: ISetFilter) {
   yield put({ type: ActionTypes.SET_FILTER_FULFILLED, payload: action.option});
 }
 
+function* closeDetails(action: ICloseDetails) {
+  yield put({ type: ActionTypes.CLOSE_DETAILS_FULFILLED });
+}
+
 export default [
   takeEvery(ActionTypes.SET_LOCATION, setLocation),
   takeEvery(ActionTypes.SET_FILTER, setFilter),
+  takeEvery(ActionTypes.CLOSE_DETAILS, closeDetails),
 ];
