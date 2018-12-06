132f99b4f2c1f014bedf26a754fde4805f532876
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-06 11:49:19 +0330

    feat: close button just added to the Information component.

--- src/ui/components/Information/index.tsx
+++ src/ui/components/Information/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 const style = require('./style.scss');
 import {IPlace} from "store/map/models";
+import {closeDetails} from "store/map/actions";
 
 interface IInformationProps {
   detail: IPlace,
+  close: () => void;
 }
 interface IInformationState {
   title: string;
@@ -13,10 +15,15 @@ export class Information extends React.PureComponent<IInformationProps, IInforma
   constructor(props: IInformationProps) {
     super(props);
   }
+
   render() {
     return (
       <div className={style.information}>
         <div className={style.informationContainer}>
+          <span
+            className={style.closeButton}
+            onClick={() => this.props.close()}
+          >&#x2716;</span>
           <p className={style.headline}>{this.props.detail.headline}</p>
           <hr/>
           <p><strong>{this.props.detail.description}</strong></p>
