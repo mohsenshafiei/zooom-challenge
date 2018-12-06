73cc63f9c0ffc03d303416d0d3ab0b6deaf93521
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-12-06 15:52:46 +0330

    refactor: some minor changes in resolve paths and code cleanup.

--- src/store/user/saga.ts
+++ src/store/user/saga.ts
@@ -1,10 +1,9 @@
 import { takeEvery, call } from 'redux-saga/effects';
 import  { ActionTypes } from './actionTypes';
-import User from 'services/api/User';
+import User from 'services/api/user';
 import { ILoginUser } from "./models";
 
 function* loginUser(action: ILoginUser) {
-  console.log('mohsen');
   const res = yield call(User.login, {
     username: action.username,
     password: action.password,
