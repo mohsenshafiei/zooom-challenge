bf14cc0438c679b4f2621df3ede321c7f038036e
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-01-17 09:29:23 +0330

    refactor: locations list just moved to the static directory.

---
+++ src/static/locations.ts
@@ -0,0 +1,69 @@
+
+export const locations = [
+  {
+    headline: "Zooom Productions",
+    description: "Building Better Brands",
+    address: "Felderstraße 12, 5330 Fuschl am See, Austria",
+    zip: "123-345-1231",
+    country: "Austria",
+    startDate: "",
+    endDate: "",
+    category: 1,
+    location: [13.3000213, 47.7954466]
+  },
+  {
+    headline: "Hauser Kaibling",
+    description: "CQ6H+JV Haus im Ennstal, Austria",
+    address: "Erzherzog-Johann-Straße 180, 8967 Haus, Austria",
+    zip: "414-399-7878",
+    country: "Austria",
+    startDate: "",
+    endDate: "",
+    category: 1,
+    location: [13.7642618, 47.4108547]
+  },
+  {
+    headline: "Campo San Toma",
+    description: "Nice place to walk in area",
+    address: "30100 Venice, Metropolitan City of Venice, Italy",
+    zip: "455-333-889",
+    country: "Italy",
+    startDate: "23 March 2018",
+    endDate: "12 October 2017",
+    category: 2,
+    location: [12.3260093, 45.4359062]
+  },
+  {
+    headline: "Zoo Zurich",
+    description: "What a nice place to watch animals",
+    address: "Zürichbergstrasse 221, 8044 Zürich, Switzerland",
+    zip: "567-123-4578",
+    country: "Switzerland",
+    startDate: "12 June 2018",
+    endDate: "23 October 2018",
+    category: 1,
+    location: [8.4666751, 47.3774337]
+  },
+  {
+    headline: "Hormozan Complex",
+    description: "",
+    address: "",
+    zip: "",
+    country: "Iran",
+    startDate: "12 July 2018",
+    endDate: "3 November 2018",
+    category: 1,
+    location: [51.3656438, 35.7581167]
+  },
+  {
+    headline: "RedBull",
+    description: "QHJP+GC Atlanta, Georgia",
+    address: "976 Brady Ave NW suite 400, Atlanta, GA 30318",
+    zip: "717-838-4465",
+    country: "USA",
+    startDate: "26 Oct 2018",
+    endDate: "4 November 2018",
+    category: 1,
+    location: [13.3000213, 47.7954466]
+  }
+];
